ea2efb6bcdd886b579bca07bf111661175d961fb
Oleksii <lesha.avdeev@gmail.com>
2023-04-18 16:05:25 +0300

    Optimize capitalize

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,7 +1,3 @@
 export default function capitalize(str) {
-  let cap = "";
-  if (str) {
-    cap += str[0].toUpperCase() + str.slice(1);
-  }
-  return cap;
+  return str ? str[0].toUpperCase() + str.slice(1) : "";
 }
